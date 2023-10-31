120efd5c7090a9b915de5910ad1550c9cb837661
flyhighmk55 <flyhighmk55@gmail.com>
2023-10-31 15:28:14 +0900

    TEST

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 
 // AOS 초기화
-  
+
 AOS.init({
   offset: 200,
   delay: 0,
@@ -11,12 +11,12 @@ AOS.init({
 
 
 // 반응형 너비 인식 재측정---------------------------------
-  
+
 var ww = $(window).outerWidth();
-$(window).resize(function(){
+$(window).resize(function () {
   ww = $(window).outerWidth();
   //console.log(ww);
-  location.reload();
+  // location.reload();
 });
 
 
@@ -46,13 +46,13 @@ $(window).resize(function(){
 
 
 //[animate(hover:none)] 네비 버튼을 클릭하면 네비 ON
-$('.header__right-btn').on('click', function(){
-    $('html, body, .header__center, .header__right-btn').toggleClass('on');
+$('.header__right-btn').on('click', function () {
+  $('html, body, .header__center, .header__right-btn').toggleClass('on');
 });
- 
+
 //[animate(hover:none)] 네비 이외 클릭하면(클릭한 곳이 .nav가 아니면) 네비 OFF
-$('.header__center').on('click', function(e){
-  if($('.nav').has(e.target).length == 0 ){ //e.target은 이벤트를 촉발시킨 엘리먼트를 말한다. 클릭 이벤트가 발생했을 때, 클릭이벤트를 촉발시킨 엘리먼트를(직접적으로 클릭된 엘리먼트) .nav가 가지고 있으면(e.target이 '.nav' 또는 '.nav'의 자식이면) 함수를 실행하지 않고, 가지고 있으면(e.target이 '.nav'의 자식이 아니면) 함수를 실행해라.
+$('.header__center').on('click', function (e) {
+  if ($('.nav').has(e.target).length == 0) { //e.target은 이벤트를 촉발시킨 엘리먼트를 말한다. 클릭 이벤트가 발생했을 때, 클릭이벤트를 촉발시킨 엘리먼트를(직접적으로 클릭된 엘리먼트) .nav가 가지고 있으면(e.target이 '.nav' 또는 '.nav'의 자식이면) 함수를 실행하지 않고, 가지고 있으면(e.target이 '.nav'의 자식이 아니면) 함수를 실행해라.
     $('html, body, .header__center, .header__right-btn').removeClass('on');
   }
   //console.log($('.nav').has(e.target).length); //1 0
@@ -63,14 +63,14 @@ $('.header__center').on('click', function(e){
 
 
 //[animation] 스크롤의 높이가 section으로 넘어가면 헤더의 색을 바꿔라
-$(window).scroll(function(){
-    var offset = $('section').offset().top;
-    var top = $('body, html').scrollTop();
-    if(offset <= top ){
-        $('.header').addClass('scroll');
-    } else {
-        $('.header').removeClass('scroll');
-    }
+$(window).scroll(function () {
+  var offset = $('section').offset().top;
+  var top = $('body, html').scrollTop();
+  if (offset <= top) {
+    $('.header').addClass('scroll');
+  } else {
+    $('.header').removeClass('scroll');
+  }
 });
 
 
@@ -84,8 +84,8 @@ $(window).scroll(function(){
 
 //[style] 번들 아이콘 배치 footer
 var ir1_l = $('.footer__sns li').length;
-for (var i = 0; i < ir1_l; i++){
-  $('.footer__sns li').eq(i).find('a').css({'background-position': 'left ' + (-40*i) + 'px top -210px'});
+for (var i = 0; i < ir1_l; i++) {
+  $('.footer__sns li').eq(i).find('a').css({ 'background-position': 'left ' + (-40 * i) + 'px top -210px' });
 };
 
 
