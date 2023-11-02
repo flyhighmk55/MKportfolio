3900e5d56996e542cb1df6da938438f5b63bcc3f
flyhighmk55 <flyhighmk55@gmail.com>
2023-11-02 11:47:51 +0900

    test

--- js/project.js
+++ js/project.js
@@ -19,21 +19,20 @@ console.log('calc(100vh - '+ hf_h +'px)');
 var project_prev = $('.project__item').prevAll();
 $('.project-main__cont').append(project_prev);
 
-
 var popup_prev = $('.project-popup__cont').prevAll();
 $('.poject-popup__frame').append(popup_prev);
 
 
 
-
-  $('#project-popup').scroll(function(){
+//팝업창에서 스크롤이 발생되면 AOS 스크롤 기준점을 팝업창으로 바꿔라 
+$('#project-popup').scroll(function(){
     AOS.refresh();
-  }); 
+}); 
 
 
 
 
-
+//프로젝트 아이템이 클릭되면 팝업창을 활성화하라
 $('.project__item').click(function(){
   //html의 스크롤을 멈추고
   $('html').css('overflow-y', 'hidden');
@@ -54,16 +53,20 @@ $('.project__item').click(function(){
 });
 
 
-
+//팝업창 밖을 클릭하면 팝업창이 사라지게 하라
 $('#project-popup').on('click', function (e) {
   if ($('.poject-popup__frame').has(e.target).length == 0) { 
+    //팝업창을 안보이게 하고
     $('#project-popup, .project-popup__cont').removeClass('on');
+    
+    //html의 스크롤을 다시 활성화 하고
     $('html').css('overflow-y', 'visible');
 //    $("#project-popup").animate({'scrollTop':0},0);
 //    AOS.init({
 //    once: false,
 //    });
-$('#project-popup').load(location.href + '#project-popup');
+    //팝업창을 새로고침하여 다시 열였을 때 AOS가 작동할 수 있도록 준비해라 
+    $('#project-popup').reload(location.href + '#project-popup');
 //      location.reload('true');
 //    location.href = location.href; 
   }
