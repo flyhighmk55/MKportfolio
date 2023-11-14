8a53cb267d03c07d75bd7a4a84e8e1b659d003b9
flyhighmk55 <flyhighmk55@gmail.com>
2023-11-14 15:41:03 +0900

    fixed

--- js/resume.js
+++ js/resume.js
@@ -2,3 +2,27 @@
 if(ww >= 768){
   $('header .nav__btn').eq(2).addClass('on');
 }
+
+
+
+//
+
+  if (ww <= 767 ){
+    $('.resume__fraim').clone().prependTo('body');
+    $('body > .resume__fraim').removeClass('on');
+  }
+
+
+
+
+
+$('.resume__fraim').click(function(){
+  if (ww <= 600 ){      
+    $('body > .resume__fraim').toggleClass('on');
+    $('html, body').toggleClass('scrollcontrol');
+
+    
+    $('body > .resume__fraim').css({ 'top': ($(window).scrollTop()), 'left':($(window).scrollLeft()) });
+  }
+});
+
