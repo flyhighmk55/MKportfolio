854d4fdb74aacb5b74c13b2b32b37ccaa4ca6bd9
flyhighmk55 <flyhighmk55@gmail.com>
2024-02-22 17:26:12 +0900

    add

--- demo-YT/js/script.js
+++ demo-YT/js/script.js
@@ -372,9 +372,6 @@ $('.main__item-desc').append('<button class="main__desc-more"><div class="icon-c
 //오피셜 마크 자동 생성
 $('.main__text-user.e_official').append('<span class="main__text-user-official"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" focusable="false" style="pointer-events: none; display: block; width: 100%; height: 100%;"><path d="M12 2C6.5 2 2 6.5 2 12s4.5 10 10 10 10-4.5 10-10S17.5 2 12 2zM9.8 17.3l-4.2-4.1L7 11.8l2.8 2.7L17 7.4l1.4 1.4-8.6 8.5z"></path></svg></span>');
 
-
-
-
 //iframe 기본형태 자동삽입
 $('.video-container').append('<iframe width="100%" height="100%" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen loading="lazy"></iframe>');
 
